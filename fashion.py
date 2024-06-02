6b2c1ca2286e4e8288ff5102f7f71118d659d998
taeyoung03 <nohty0404@kakao.com>
2024-06-02 20:56:50 +0900

    Feat

---
+++ fashion.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import numpy as np
+
+(train_x, train_y), (test_x, test_y) = tf.keras.datasets.fashion_mnist.load_data()
+
+train_x = train_x / 255.0
+test_x = test_x / 255.0
+
+train_x = train_x.reshape((train_x.shape[0], 28, 28, 1))
+test_x = test_x.reshape((test_x.shape[0], 28, 28, 1))
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankleboot']
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), padding="same", activation="relu", input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation="relu"),
+    tf.keras.layers.Dense(10, activation="softmax"),
+])
+
+model.summary()
+
+model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+model.fit(train_x, train_y, validation_data=(test_x, test_y), epochs=5)
+
+# score = model.evaluate(test_x, test_y)
+# print(score)
